a5bf95405ec25769c47bf063f546c3ffbfa1c3ef
Will <Willsandersnpt@gmail.com>
2024-12-27 22:54:43 -0500

    Created meal log table and insert weight query

---
+++ sql/insert_weight.sql
@@ -0,0 +1,1 @@
+insert into weight(userid, weight, measuredate) values(?, ?, ?)
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ def initDatabase(util:SqliteUtil):
     util.executeSqlFromFile("create_user")
     util.executeSqlFromFile("create_meal")
     util.executeSqlFromFile("create_weight")
+    util.executeSqlFromFile("create_meallog")
     # TODO: create meal log
 
 def insertUser(util:SqliteUtil, name:list):
@@ -15,8 +16,9 @@ def insertMeal(util:SqliteUtil, username:str, m:str, cal:int, carb:int, f:int, p
     util.executeSqlFromFileWithParam('insert_meal', t)
 
 # TODO
-def insertWeight():
-    pass
+def insertWeight(util:SqliteUtil, userid:int, weight:int, measuredate:int):
+    t = [userid, weight, measuredate]
+    util.executeSqlFromFileWithParam('insert_weight', t)
 
 # TODO
 def insertMealLog():
@@ -24,13 +26,14 @@ def insertMealLog():
 
 def main():
     sql = SqliteUtil()
-    # initDatabase(sql)
+    initDatabase(sql)
     # insertUser(sql, 'Bob')
     # insertMeal(sql, 'Bob', 'Philly Cheesesteak', 604, 50, 40, 40)
-    print(sql.queryToList('''select * from user'''))
-    res = sql.queryToVal("""select id from user where name = 'Will'""", bool)
-    print(res , type(res))
-    print(sql.queryToDict("""select * from meal"""))
+    # print(sql.queryToList('''select * from user'''))
+    # res = sql.queryToVal("""select id from user where name = 'Will'""", bool)
+    # print(res , type(res))
+    # print(sql.queryToDict("""select * from meal"""))
+    insertWeight(sql, 1, 170, '12/27/2024')
 
 
 
