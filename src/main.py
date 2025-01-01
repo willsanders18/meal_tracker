a0e6279ca3a81f3f10f12fb1a16885c63327e3bb
Will <Willsandersnpt@gmail.com>
2024-12-31 19:56:11 -0500

    Created basic while loop

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def main():
     track = Tracker(sql)
     track.initDatabase()
     # initDatabase(sql)
-    track.insertUser('Will', 21, 73, 'male')
+    # track.insertUser('Will', 21, 73, 'male')
     # # insertMeal(sql, 'Bob', 'Philly Cheesesteak', 604, 50, 40, 40)
     # # print(sql.queryToList('''select * from user'''))
     # # res = sql.queryToVal("""select id from user where name = 'Will'""", bool)
@@ -15,7 +15,9 @@ def main():
     # # print(sql.queryToDict("""select * from meal"""))
     # # insertMealLog(sql, 'Will', 'Philly Cheesesteak', '2024-12-28 00:09:00')
     # insertWeight(sql, 'Will', 185, '2024-12-28 00:28:00')
-
+    while True:
+        print('Test')
+        break
 
 if __name__ == "__main__":
     main()
