2ad15daf9b6598f926b47b925729c029c959da64
Will <Willsandersnpt@gmail.com>
2024-12-31 21:03:31 -0500

    Added ability to login and create new user

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
 from sqliteutil import SqliteUtil
 from mealtracker import Tracker
 
+def newUser(tr:Tracker):
+    n = str(input('Enter your name: '))
+    a = int(input('Enter your age: '))
+    h = int(input('Enter your height in inches: '))
+    s = str(input('Enter your sex: '))
+    tr.insertUser(n, a, h, s)
 
 def main():
     sql = SqliteUtil()
     track = Tracker(sql)
     track.initDatabase()
-    # initDatabase(sql)
-    # track.insertUser('Will', 21, 73, 'male')
-    # # insertMeal(sql, 'Bob', 'Philly Cheesesteak', 604, 50, 40, 40)
-    # # print(sql.queryToList('''select * from user'''))
-    # # res = sql.queryToVal("""select id from user where name = 'Will'""", bool)
-    # # print(res , type(res))
-    # # print(sql.queryToDict("""select * from meal"""))
-    # # insertMealLog(sql, 'Will', 'Philly Cheesesteak', '2024-12-28 00:09:00')
-    # insertWeight(sql, 'Will', 185, '2024-12-28 00:28:00')
     while True:
-        print('Test')
+        username = str(input('Enter your name: '))
+        # if username.capitalize()
+        names = track.util.queryToList(track.util._getSqlFromFile('select_all_names'))
+        namesList = []
+
+        for i in names:
+            namesList.append(i[0])
+        
+        if username.capitalize() in namesList:
+            print(f'{username} recognized')
+        else:
+            ask = str(input(f'{username} not recognized, would you like to create a new user? (y/n): '))
+            if ask.lower() == 'y':
+                newUser(track)
+            elif ask.lower == 'n':
+                print('Thank you, quitting program...')
+                break
+            else:
+                print('Invalid response, ending test...')
+                break
         break
 
 if __name__ == "__main__":
