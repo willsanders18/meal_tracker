446efea1c6900044b4895148e12dc780fb179d39
Will <Willsandersnpt@gmail.com>
2025-01-01 21:25:22 -0500

    Small changes to user input

--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
 from sqliteutil import SqliteUtil
 from mealtracker import Tracker
 
-def newUser(tr:Tracker):
-    n = str(input('Enter your name: '))
-    a = int(input('Enter your age: '))
-    h = int(input('Enter your height in inches: '))
-    s = str(input('Enter your sex: '))
-    tr.insertUser(n, a, h, s)
 
 def main():
     sql = SqliteUtil()
@@ -15,18 +9,14 @@ def main():
     while True:
         username = str(input('Enter your name: '))
         # if username.capitalize()
-        names = track.util.queryToList(track.util._getSqlFromFile('select_all_names'))
-        namesList = []
-
-        for i in names:
-            namesList.append(i[0])
+        names = track.getAllNames()
         
-        if username.capitalize() in namesList:
+        if username.capitalize() in names:
             print(f'{username} recognized')
         else:
             ask = str(input(f'{username} not recognized, would you like to create a new user? (y/n): '))
             if ask.lower() == 'y':
-                newUser(track)
+                track.newUser()
             elif ask.lower == 'n':
                 print('Thank you, quitting program...')
                 break
